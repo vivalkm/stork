e890b3d74d007e10993293c4f07b4e9ad0c779a8
vivalkm <vivalkm@gmail.com>
2023-08-16 15:00:36 -0700

    Update header color and logo image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import useAuthContext from "../hooks/useAuthContext";
-import logo from "../assets/logo.png"
+import logo from "../assets/logo.png";
 
 export default function Header() {
     const location = useLocation();
@@ -12,23 +12,23 @@ export default function Header() {
     const { loggedIn } = useAuthContext();
 
     return (
-        <div className="border-b bg-white shadow-sm sticky top-0 z-40">
+        <div className="border-b shadow-sm sticky top-0 z-40 header">
             <header className="flex justify-between items-center">
                 <div>
                     <img
-                        className="cursor-pointer header"
+                        className="cursor-pointer ml-2 header-logo"
                         src={logo}
                         alt="logo"
                         onClick={() => navigate("/")}
                     />
                 </div>
-                <div>
+                <div className="mr-2">
                     <ul className="flex space-x-10">
                         <li
                             className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${
                                 pathMatchRoute("/")
-                                    ? "text-black border-b-red-500"
-                                    : "text-gray-400 border-b-transparent"
+                                    ? "text-amber-200 border-amber-200"
+                                    : "text-white border-b-transparent"
                             }`}
                             onClick={() => navigate("/")}
                         >
@@ -37,8 +37,8 @@ export default function Header() {
                         <li
                             className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${
                                 pathMatchRoute("/offers")
-                                    ? "text-black border-b-red-500"
-                                    : "text-gray-400 border-b-transparent"
+                                    ? "text-amber-200 border-amber-200"
+                                    : "text-white border-b-transparent"
                             }`}
                             onClick={() => navigate("/offers")}
                         >
@@ -47,8 +47,8 @@ export default function Header() {
                         <li
                             className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${
                                 pathMatchRoute("/sell")
-                                    ? "text-black border-b-red-500"
-                                    : "text-gray-400 border-b-transparent"
+                                    ? "text-amber-200 border-amber-200"
+                                    : "text-white border-b-transparent"
                             }`}
                             onClick={() => navigate("/sell")}
                         >
@@ -57,8 +57,8 @@ export default function Header() {
                         <li
                             className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${
                                 pathMatchRoute("/signin") || pathMatchRoute("/profile")
-                                    ? "text-black border-b-red-500"
-                                    : "text-gray-400 border-b-transparent"
+                                    ? "text-amber-200 border-amber-200"
+                                    : "text-white border-b-transparent"
                             }`}
                             onClick={() => navigate(loggedIn ? "/profile" : "/signin")}
                         >
