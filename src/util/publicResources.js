1edf35a91ef4c5107c508d5c70f22281639e8253
Lincoln Li <vivalkm@gmail.com>
2023-08-28 21:05:38 -0700

    Change bg video host to Firebase Storage

--- src/util/publicResources.js
+++ src/util/publicResources.js
@@ -1,4 +1,5 @@
 export const defaultAvatar =
     "https://static-00.iconduck.com/assets.00/avatar-default-symbolic-icon-2048x1949-pq9uiebg.png";
 
-export const bgVideo = "https://projectstork.s3.us-west-2.amazonaws.com/ocean.mp4";
+export const bgVideo =
+    "https://firebasestorage.googleapis.com/v0/b/stork-69d6e.appspot.com/o/ocean.mp4?alt=media&token=8bc79c13-db27-40ad-91d3-a2d4f1225d6b";
